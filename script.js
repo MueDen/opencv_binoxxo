0f1b0b50c754ee538bda4361042058df96101aab
MueDen <denis.mueller@students.fhnw.ch>
2023-07-25 18:38:58 +0200

    working on detecting different contours

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import('utils.js')
-let video = document.getElementById('video')
+let video = document.getElementById('video');
 const height = 560
 const width = 720
 let streaming = false;
@@ -13,10 +12,10 @@ const startVideo = () => {
 
 let canvasFrame = document.getElementById("canvasFrame"); // canvasFrame is the id of <canvas>
 let context = canvasFrame.getContext("2d");
-let src = new cv.Mat(height, width, cv.CV_8UC4);
-let dst = new cv.Mat(height, width, cv.CV_8UC1);
 const FPS = 30;
 function processVideo() {
+    let dst = new cv.Mat.zeros(height, width, cv.CV_8UC3);
+    let src = new cv.Mat(height, width, cv.CV_8UC4);
     if(!streaming){
         return;
     }
@@ -24,14 +23,19 @@ function processVideo() {
     // draw video to the input slot on the page
     context.drawImage(video, 0, 0, width, height);
     // take the video data from input and save it to the src matrix
+    // console.log(context.getImageData(0, 0, width, height).data)
     src.data.set(context.getImageData(0, 0, width, height).data);
     
     preProcessing({source: src, destination: dst})
     // show the generated images from video in short succession for output video
     cv.imshow("canvasFrame", dst);
-    let delay = 1000/FPS - (Date.now() - begin);
+    let delay = 10000/FPS - (Date.now() - begin);
     // recursively call process Video
+    src.delete();
+    dst.delete();
     setTimeout(processVideo, delay);
+
+
 }
 // schedule first one.
 const startStream = () => {
@@ -47,8 +51,7 @@ const stopStream = () => {
     video.srcObject = null;
     document.getElementById('video').style.display = 'none';
     document.getElementById('canvasFrame').style.display = 'none';
-    // src.delete();
-    // dst.delete();
+
     return
 }
 
--- utils.js
+++ utils.js
@@ -1,20 +1,55 @@
+let once = false;
+
 const preProcessing = ({source: src, destination: dst}) => {
-        // // Manipulate src data and push it dst matrix
-    cv.GaussianBlur(src,dst,new cv.Size(5,5),0)
-    let gray = cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-    cv.Canny(src, dst, 50, 100, 3, false)
-    // return cv2.GaussianBlur(src,(5,5),0)
-    // gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    let mask = new cv.Mat(src.size().height, src.size().width, cv.CV_8UC4);
-    // console.log(mask)
 
-    // mask = np.zeros((gray.shape),np.uint8)
-    // kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
+    // apply gaussian blur and grayscale to simplify image information
+    cv.GaussianBlur(src, src, new cv.Size(5,5), 0); 
+    let gray = new cv.Mat();
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
 
-    // close = cv2.morphologyEx(gray,cv2.MORPH_CLOSE,kernel1)
-    // div = np.float32(gray)/(close)
-    // res = np.uint8(cv2.normalize(div,div,0,255,cv2.NORM_MINMAX))
-    // res2 = cv2.cvtColor(res,cv2.COLOR_GRAY2BGR)
+    let mask = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC1);
+    
+    let close = new cv.Mat();
+    // Kernel for morphological transformation
+    let kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(11,11));
+    cv.morphologyEx(gray, close, cv.MORPH_CLOSE, kernel);
+
+    let div = new cv.Mat();
+    gray.convertTo(gray, cv.CV_32FC1);
+    cv.divide(gray, close, div, 1, cv.CV_32FC1);
+
+    cv.normalize(div, div, 0, 255, cv.NORM_MINMAX);
+
+    let normalized = new cv.Mat();
+    div.convertTo(normalized, cv.CV_8UC1)
+
+    let thresh = new cv.Mat();
+    cv.adaptiveThreshold(normalized, thresh,255,0,1,19,2);
+
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    // You can try more different parameters
+    cv.findContours(thresh, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+    // draw contours with random Scalar
+    let max_area = 0;
+    let best_cnt = 0;
+    for (let i = 0; i < contours.size(); ++i) {
+        let area = cv.contourArea(contours.get(i));
+        if (area > 100 && area > max_area){
+            max_area = area;
+            best_cnt = i;
+        }
+    }
+
+    // draw the largest contour
+    cv.drawContours(mask,contours,best_cnt,new cv.Scalar(255, 255, 255),-1);
+    cv.drawContours(mask,contours,best_cnt,new cv.Scalar(0, 0, 0),2);
+
+    cv.bitwise_and(normalized, mask, normalized);
+    let kernelx = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(2,10));
+
+    let dx = new cv.Mat();
+    cv.Sobel(normalized, dx, cv.CV_16S,1,0);
 
 
 }
